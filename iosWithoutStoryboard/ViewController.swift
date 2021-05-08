931a07ef675e5de6bf05f90fbac3d62d2160f58c
sjc-bui <bui@setjapan.co.jp>
2021-05-08 15:01:22 +0900

    add libs

--- iosWithoutStoryboard/ViewController.swift
+++ iosWithoutStoryboard/ViewController.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UITableViewDelegate {
     let tableView = UITableView()
     var safeArea: UILayoutGuide!
 
-    let characters = ["東京", "名古屋", "大阪", "宮城", "広島", "川崎", "沖縄"]
+    let url = "http://data.fixer.io/api/latest"
+    let accessKey = "8ef64676f4bca80be997020a5936255b"
+    let base = "EUR"
+
+    var currency = [String]()
 
     override func loadView() {
         super.loadView()
         view.backgroundColor = .white
         safeArea = view.layoutMarginsGuide
+
         tableView.delegate = self
         tableView.dataSource = self
+
+        let params = ["access_key": accessKey, "base": base]
+        getCurrency(url: url, params: params)
         setTableView()
     }
 
@@ -31,19 +41,40 @@ class ViewController: UIViewController, UITableViewDelegate {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+
+    func getCurrency(url: String, params: [String: String]) {
+        AF.request(url, method: .get, parameters: params).responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                let jsonData: JSON = JSON(response.data!)
+                self.updateTableData(data: jsonData)
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
+            self.tableView.reloadData()
+        }
+    }
+
+    func updateTableData(data: JSON) {
+        for (name, price) in data["rates"] {
+            let cur = "\(name) \(price)"
+            currency.append(cur)
+        }
+    }
 }
 
 extension ViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return characters.count
+        return currency.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        cell.imageView?.image = UIImage(named: "swift")
-        cell.textLabel?.text = characters[indexPath.row]
-        cell.detailTextLabel?.text = "detail text"
+        //cell.imageView?.image = UIImage(named: "swift")
+        cell.textLabel?.text = currency[indexPath.row]
+        cell.detailTextLabel?.text = "Currency"
         cell.accessoryType = .disclosureIndicator
         return cell
     }

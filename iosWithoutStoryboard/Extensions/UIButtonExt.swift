1f22e8d7b58f50b7add3168ec8065c3b19a9206b
sjc-bui <bui@setjapan.co.jp>
2021-05-11 14:05:52 +0900

    edit button style

--- iosWithoutStoryboard/Extensions/UIButtonExt.swift
+++ iosWithoutStoryboard/Extensions/UIButtonExt.swift
@@ -10,13 +10,13 @@ import UIKit
 extension UIButton {
     func RoundAndWeight(title: String, size: CGFloat, style: UIFont.Weight, background: UIColor) {
         self.setTitle(title, for: .normal)
-        self.layer.cornerRadius = 4
+        self.layer.cornerRadius = 6
         self.titleLabel?.font = UIFont.systemFont(ofSize: size, weight: style)
         self.backgroundColor = background
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         self.layer.shadowRadius = 0.0
-        self.layer.shadowOpacity = 1.0
+        self.layer.shadowOpacity = 0.5
         self.layer.masksToBounds = false
     }
 }
